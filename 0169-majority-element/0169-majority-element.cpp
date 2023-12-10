c8982dee6337f46a2f1b1d223d8164c57e67de5e
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2023-12-10 19:42:54 +0530

    Time: 15 ms (51.53%), Space: 20 MB (27.43%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,18 +1,35 @@
-class Solution {
-public:
-    int majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        unordered_map<int,int> um;
-        
-        for(int num: nums){
-            um[num]++;
+class Solution
+{
+    public:
+        int majorityElement(vector<int> &nums)
+        {
+           int count = 0, candidate = -1;
+            for(int num : nums){
+                if(count == 0){
+                    candidate = num;
+                }
+                if(candidate == num)
+                    count++;
+                else
+                    count--;
+            }          
+            return candidate;          
         }
-        
-        for(auto ele: um){                   
-            if(ele.second > n/2 )
-                return ele.first;
-        }
-        
-       return -1; 
-    }
 };
+
+/*int n = nums.size();
+unordered_map<int, int> um;
+
+for (int num: nums)
+{
+    um[num]++;
+}
+
+for (auto ele: um)
+{
+    if (ele.second > n / 2)
+        return ele.first;
+}
+
+return -1;
+}*/
