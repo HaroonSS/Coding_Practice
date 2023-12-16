5af86518dd58d15d6112b7438ded06847f0caeea
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2023-12-16 19:38:58 +0530

    Time: 4 ms (80.54%), Space: 8 MB (26.69%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        
+        string S = "";
+        
+        for(auto c : s){
+            if(c >= 'A' && c <= 'Z')
+                S.push_back(c + 32);
+            else if((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9'))
+                S.push_back(c);
+        }
+        
+        int left = 0, right = S.size() - 1;
+        
+        while(left < right){
+            if(S[left++] != S[right--]){
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
