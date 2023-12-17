df2dad24f7e70274f4ddf4212519c855afa205e4
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2023-12-17 11:37:36 +0530

    Time: 4 ms (43.66%), Space: 6.9 MB (91.20%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        
+        int countArr[256] = {0};
+        int maxLength = 0, oddChar =0;
+        
+        for(char c : s){
+            countArr[c]++;
+        }
+        
+        for(int i=0; i< 256; i++){
+            if(countArr[i] % 2 ==0){
+                maxLength += countArr[i];
+            }
+            else if(countArr[i] >= 1){
+                maxLength += countArr[i] -1;
+                oddChar ++;
+            }
+        }
+        if(oddChar > 0 )
+             maxLength ++ ;
+       return maxLength; 
+    }
+};
