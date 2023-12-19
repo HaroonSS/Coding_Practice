3f888e8ea7f3964284b31d4dfb42d2bd5bfa7ff1
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2023-12-19 21:07:33 +0530

    Time: 11 ms (68.45%), Space: 9 MB (60.29%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        vector<int> freq_p(26), freq_s(26);
-        for(auto &it:p) freq_p[it-'a']++;
+       vector<int> freq_p(26), freq_s(26);
+        int pLength = p.size(), sLength = s.size();
+        for(auto it:p) 
+            freq_p[it-'a']++;
 
         int low=0;
         vector<int> res;
-        for(int high=0; high<s.size(); high++)
+        for(int high=0; high<sLength; high++)
         {
             freq_s[s[high]-'a']++;
-            if((high-low+1==p.size()) && (freq_p==freq_s)) // vector compared
+            if((high-low+1==pLength) && (freq_p==freq_s)) // vector compared
                 res.push_back(low);
 
-            if(high-low+1 >= p.size())
+            if(high-low+1 >= pLength)
             {
                 freq_s[s[low]-'a']--;
                 low++;
             }
         }
         return res;
-        
     }
 };
