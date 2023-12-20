fe397ba3be12190b3e3b64e37fca0403c40031e7
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2023-12-20 23:08:24 +0530

    Time: 3 ms (90.25%), Space: 14.2 MB (40.41%) - LeetHub

---
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int maxi = INT_MIN;
+        int prod=1;
+
+        for(int i=0;i<nums.size();i++)
+        {
+          prod*=nums[i];
+          maxi=max(prod,maxi);
+          if(prod==0)
+           prod=1;
+        }
+        prod=1;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+          prod*=nums[i];
+
+          maxi=max(prod,maxi);
+          if(prod==0)
+           prod=1;
+        }
+        return maxi;
+    }
+
+};/*
+The Approach to this question is exactly same as Kadane's Algo the only differece is, we will be traversing the array from both sides i.e left to right ( lets say the max prod is maxi1 ) and from right to left ( say max prod is maxi2 ), now the final answer will we max of maxi1 and maxi2.
+
+Ques : Why do we have a need to traverse from right to left ??
+Ans : lets take an example
+arr {-8,5,3,1,6}
+
+// By traversing left to right we have an answer maxi1.
+maxi1 = -720 , which is clearly not the max prod value.
+-> The whole concept lies in this statement - As we can see that there is only 1 negative no. ( if we consider that no. then the final answer can never be positive ), therefore to avoid this situation we also traverse from right to left to get the final maximum prod.
+*/
