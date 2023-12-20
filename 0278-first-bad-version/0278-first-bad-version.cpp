adc0a405b18187fd9237c5657410c29a60ffcf7a
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2023-12-20 19:12:24 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (35.57%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -21,3 +21,13 @@ public:
         
     }
 };
+/*
+    int firstBadVersion(int n) {
+        int lo = 1, hi = n, mid;
+        while (lo < hi) {
+            mid = lo + (hi - lo) / 2;
+            if (isBadVersion(mid)) hi = mid;
+            else lo = mid+1;
+        }
+        return lo;
+    }*/
