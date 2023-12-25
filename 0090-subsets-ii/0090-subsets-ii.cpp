09767996ef37aeb32afa21a1212dc6b0d821c217
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2023-12-25 19:08:58 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (92.25%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+    void helper(int ind, vector<vector < int>> &res, vector< int > &temp, vector< int > &nums)
+    {
+        res.push_back(temp);
+
+        for (int i = ind; i < nums.size(); ++i)
+        {
+            if (i != ind && nums[i] == nums[i - 1]) continue;
+            temp.push_back(nums[i]);
+            helper(i + 1, res, temp, nums);
+            temp.pop_back();
+        }
+    }
+    public:
+        vector<vector < int>> subsetsWithDup(vector<int> &nums)
+        {
+            vector<vector < int>> res;
+            vector<int> temp;
+            sort(nums.begin(), nums.end());
+            helper(0, res, temp, nums);
+            return res;
+        }
+};
+/*https://takeuforward.org/data-structure/subset-sum-sum-of-all-subsets/
+https://takeuforward.org/data-structure/subset-ii-print-all-the-unique-subsets/*/
