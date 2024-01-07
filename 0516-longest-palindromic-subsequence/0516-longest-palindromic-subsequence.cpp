ad1e84405b697a58febc657e8c0666dcf1832aa1
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2024-01-07 19:47:59 +0530

    Time: 171 ms (32.90%), Space: 73.5 MB (22.35%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int lcs(string s1, string s2) {
         //The longest palindromic subsequence of a string is the longest common subsequence of the given string and its reverse.
-    int n = s1.size();
-    int m = s2.size();
+    int n = s1.size(); int m = s2.size();
 
     vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
 
@@ -22,17 +21,18 @@ public:
                 dp[ind1][ind2] = max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
         }
     }
-
     return dp[n][m];
 }
-    int lcsa(string s1, string s2) {
-    int n = s1.size();
+    int longestPalindromeSubseq(string s) {
+    string t = s;
+    reverse(t.begin(), t.end());// Create a reversed copy of the string
+    return lcs(s, t);    // Call the LCS function to find the length of the Longest Common Subsequence
+    }
+};/*
+    int lcs(string s1, string s2) {
     int m = s2.size();
 
-    // Create two arrays to store the previous and current rows of DP values
-    vector<int> prev(m + 1, 0), cur(m + 1, 0);
-
-    // Base Case is covered as we have initialized the prev and cur to 0.
+    vector<int> prev(m + 1, 0), cur(m + 1, 0);// Base Case is covered as we have initialized the prev and cur to 0.
 
     for (int ind1 = 1; ind1 <= n; ind1++) {
         for (int ind2 = 1; ind2 <= m; ind2++) {
@@ -41,17 +41,9 @@ public:
             else
                 cur[ind2] = max(prev[ind2], cur[ind2 - 1]);
         }
-        // Update the prev array with the current values
         prev = cur;
     }
 
     // The value at prev[m] contains the length of the LCS
     return prev[m];
-}
-    int longestPalindromeSubseq(string s) {
-    // Create a reversed copy of the string
-    string t = s;
-    reverse(t.begin(), t.end());
-    return lcs(s, t);    // Call the LCS function to find the length of the Longest Common Subsequence
-    }
-};
+}*/
