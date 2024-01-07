6d20211a71bfab766c83cc0f8f277db40f65556c
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2024-01-07 12:18:07 +0530

    Time: 48 ms (61.75%), Space: 24.9 MB (36.97%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -0,0 +1,94 @@
+class Solution {
+public:
+int longestCommonSubsequence(string s1, string s2) {
+     int n=s1.size();
+    int m=s2.size();
+
+    vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+    for(int i=0;i<=n;i++){
+        dp[i][0] = 0;
+    }
+    for(int i=0;i<=m;i++){
+        dp[0][i] = 0;
+    }
+    
+    for(int ind1=1;ind1<=n;ind1++){
+        for(int ind2=1;ind2<=m;ind2++){
+            if(s1[ind1-1]==s2[ind2-1])
+                dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
+            else
+                dp[ind1][ind2] = 0 + max(dp[ind1-1][ind2],dp[ind1][ind2-1]);
+        }
+    }
+    
+    return dp[n][m];
+        
+    }
+}; /*
+Shifting of indexes :
+    In the recursive logic, we set the base case to if(ind1<0 || ind2<0) but we can’t set the dp array’s index to -1. Therefore a hack for this issue is to shift every index by 1 towards the right.
+
+TC: O(N*M) There are two nested loops
+SC: O(N*M) We are using an external array of size ‘N*M)’. Stack Space is eliminated.
+    
+//Memoization
+    int lcsUtil(string& s1, string& s2, int ind1, int ind2,  vector<vector<int>>& dp){
+
+    if(ind1<0 || ind2<0)
+        return 0;
+        
+    if(dp[ind1][ind2]!=-1)
+        return dp[ind1][ind2];
+    
+    if(s1[ind1] == s2[ind2])
+        return dp[ind1][ind2] = 1 + lcsUtil(s1,s2,ind1-1,ind2-1,dp);
+    
+    else 
+        return dp[ind1][ind2] = 0 + max(lcsUtil(s1,s2,ind1,ind2-1,dp), lcsUtil(s1,s2,ind1-1,ind2,dp)); 
+}
+int lcs(string s1, string s2) {
+    int n = s1.size();
+    int m = s2.size();
+    
+    vector<vector<int>> dp(n, vector<int>(m, -1)); // Create a DP table
+    return lcsUtil(s1, s2, n - 1, m - 1, dp);
+}
+
+TC: O(N*M) There are N*M states therefore at max ‘N*M’ new problems will be solved.
+    
+SC: O(N*M) + O(N+M) We are using an auxiliary recursion stack space(O(N+M)) (see the recursive tree, in the worst case, we will go till N+M calls at a time) and a 2D array ( O(N*M)).
+
+//Space Optimize
+replace dp[ind-1] to prev and dp[ind] with cur and after the inner loop executes, we will set prev = cur, so that the cur row can serve as prev for the next index.
+    
+int lcs(string s1, string s2) {
+    int n = s1.size();
+    int m = s2.size();
+
+    // Initialize two vectors to store the current and previous rows of the DP table
+    vector<int> prev(m + 1, 0), cur(m + 1, 0);
+
+    // Base case is covered as we have initialized the prev and cur vectors to 0.
+
+    for (int ind1 = 1; ind1 <= n; ind1++) {
+        for (int ind2 = 1; ind2 <= m; ind2++) {
+            if (s1[ind1 - 1] == s2[ind2 - 1])
+                cur[ind2] = 1 + prev[ind2 - 1]; // Characters match, increment LCS length
+            else
+                cur[ind2] = max(prev[ind2], cur[ind2 - 1]); // Characters don't match, consider the maximum from above or left
+        }
+        prev = cur; // Update the previous row with the current row
+    }
+
+    return prev[m]; // Return the length of the Longest Common Subsequence
+}
+Time Complexity: O(N*M) There are two nested loops.
+Space Complexity: O(M) We are using an external array of size ‘M+1’ to store only two rows.
+*/    
+    
+    
+    
+    
+    
+    
+    
