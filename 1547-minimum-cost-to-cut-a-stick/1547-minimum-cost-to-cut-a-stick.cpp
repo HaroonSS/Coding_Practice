06e6ccf3f75218ff2627f267465bf5d5bcdfc9db
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2024-01-13 20:03:51 +0530

    Time: 81 ms (38.83%), Space: 10.5 MB (73.55%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -36,4 +36,32 @@ int minCost(int n, vector<int> &cuts) {
     // Call the recursive function to find the minimum cost.
     return findMinimumCost(1, c, cuts, dp);
 }
-};
+};/*
+int minimumCost(int n, int c, vector<int> &cuts) {
+    // Modify the cuts array by adding 0 at the beginning and 'n' at the end.
+    cuts.push_back(n);
+    cuts.insert(cuts.begin(), 0);
+    sort(cuts.begin(), cuts.end());
+
+    // Create a DP table to store calculated values.
+    vector<vector<int>> dp(c + 2, vector<int>(c + 2, 0));
+
+    for (int i = c; i >= 1; i--) {
+        for (int j = 1; j <= c; j++) {
+            if (i > j) continue;
+
+            int mini = INT_MAX;
+
+            for (int ind = i; ind <= j; ind++) {
+                // Calculate the cost for making a cut at position 'ind'.
+                int ans = cuts[j + 1] - cuts[i - 1] + dp[i][ind - 1] + dp[ind + 1][j];
+
+                mini = min(mini, ans);
+            }
+
+            dp[i][j] = mini;
+        }
+    }
+
+    return dp[1][c];
+}*/
