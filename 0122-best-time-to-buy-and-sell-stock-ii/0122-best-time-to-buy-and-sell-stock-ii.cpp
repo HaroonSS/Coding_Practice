fb6e1bf6ac0818c8b5f5b9d713a1fa587b055ca5
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2024-01-13 12:27:47 +0530

    Time: 0 ms (100.00%), Space: 13.3 MB (66.50%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        /*
+            profit: maximum total profit.
+            buy: the day when buy stock.
+            sell: the day when sell stock.
+            days: maximum trade days.
+        */
+        int profit = 0, buy = 0, sell = 0, days = prices.size();
+        
+        while(buy < days && sell < days)
+        {
+            while(buy < days - 1 && prices[buy + 1] < prices[buy])
+                buy++; // find the valley of prices               
+            
+            sell = buy;
+            
+            while(sell < days - 1 && prices[sell + 1] > prices[sell])
+                sell++; // find the peak of prices
+            
+            profit += prices[sell] - prices[buy];
+                
+            buy = sell + 1;
+        }
+        
+        return profit;
+    }
+};
