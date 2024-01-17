4d822142c4c601a1f61a104ce8df3d4262b8245e
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2024-01-17 21:22:28 +0530

    Time: 35 ms (67.46%), Space: 13.6 MB (69.97%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+int largestRectangleArea(vector < int > & histo) {
+    stack < int > st;
+    int maxA = 0;
+    int n = histo.size();
+    for (int i = 0; i <= n; i++) {
+        while (!st.empty() && (i == n || histo[st.top()] >= histo[i])) {
+            int height = histo[st.top()];
+            st.pop();
+            int width;
+            if (st.empty())
+                width = i;
+            else
+                width = i - st.top() - 1;
+            maxA = max(maxA, width * height);
+        }
+        st.push(i);
+    }
+    return maxA;
+}
+    
+    int maximalRectangle(vector<vector<char>>& mat) {
+        int m = mat.size();  // Rows
+        int n = mat[0].size();  // Columns
+        int maxArea = 0;
+        vector<int> height(n, 0);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == '1') height[j]++;
+                else height[j] = 0;
+            }
+            int area = largestRectangleArea(height);
+            maxArea = max(maxArea, area);
+        }
+        return maxArea;
+    }
+};
