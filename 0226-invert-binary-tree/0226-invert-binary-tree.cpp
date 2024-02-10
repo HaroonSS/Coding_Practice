e522e32bdcb8fa58b2783db4480f21f4b30ce199
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2024-02-10 17:47:24 +0530

    Time: 5 ms (25.61%), Space: 11.6 MB (6.08%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -10,15 +10,37 @@
  * };
  */
 class Solution {
-public:
+public:/*
     TreeNode* invertTree(TreeNode* root) {
         if (root==NULL)
             return NULL;
-        TreeNode* temp=root->left;
+        TreeNode* temp=root->left;   //inorder, preorder, postorder traversal doesn't matter
         root->left=root->right;
         root->right=temp;
         invertTree(root->left);
         invertTree(root->right);
         return root;
     }
-};
+};*/
+TreeNode* invertTree(TreeNode* root) {
+    
+    if(nullptr == root) return root;
+    
+    queue<TreeNode*> myQueue;   // our queue to do BFS
+    myQueue.push(root);         // push very first item - root
+
+    while(!myQueue.empty()){    // run until there are nodes in the queue         
+        TreeNode *node = myQueue.front();  // get element from queue
+        myQueue.pop();                     // remove element from queue
+                 
+        if(node->left != nullptr){         // add left kid to the queue if it exists
+            myQueue.push(node->left);
+        }
+        
+        if(node->right != nullptr){        // add right kid 
+            myQueue.push(node->right);
+        }       
+       swap(node -> left, node -> right);
+    }
+    return root;
+}};
