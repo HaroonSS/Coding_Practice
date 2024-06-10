80ff2aceae468109021a04eda2942a4c12d30a08
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2024-06-10 23:24:03 +0530

    Time: 34 ms (45.95%), Space: 17.4 MB (50.51%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,23 +1,39 @@
 class Solution {
 public:
 int largestRectangleArea(vector < int > & histo) {
-    stack < int > st;
-    int maxA = 0;
-    int n = histo.size();
-    for (int i = 0; i <= n; i++) {
-        while (!st.empty() && (i == n || histo[st.top()] >= histo[i])) {
-            int height = histo[st.top()];
-            st.pop();
-            int width;
-            if (st.empty())
-                width = i;
-            else
-                width = i - st.top() - 1;
-            maxA = max(maxA, width * height);
+      int n = histo.size();
+      stack < int > st;
+      int leftsmall[n], rightsmall[n];
+      for (int i = 0; i < n; i++) {
+        while (!st.empty() && histo[st.top()] >= histo[i]) {
+          st.pop();
         }
+        if (st.empty())
+          leftsmall[i] = 0;
+        else
+          leftsmall[i] = st.top() + 1;
         st.push(i);
-    }
-    return maxA;
+      }
+      // clear the stack to be re-used
+      while (!st.empty())
+        st.pop();
+
+      for (int i = n - 1; i >= 0; i--) {
+        while (!st.empty() && histo[st.top()] >= histo[i])
+          st.pop();
+
+        if (st.empty())
+          rightsmall[i] = n - 1;
+        else
+          rightsmall[i] = st.top() - 1;
+
+        st.push(i);
+      }
+      int maxA = 0;
+      for (int i = 0; i < n; i++) {
+        maxA = max(maxA, histo[i] * (rightsmall[i] - leftsmall[i] + 1));
+      }
+      return maxA;
 }
     
     int maximalRectangle(vector<vector<char>>& mat) {
@@ -36,3 +52,9 @@ int largestRectangleArea(vector < int > & histo) {
         return maxArea;
     }
 };
+/*
+Time Complexity: O(N * (M+M)), where N = total no. of rows and M = total no. of columns.
+Reason: O(N) for running a loop to check all rows. Now, inside that loop, O(M) is for visiting all the columns, and another O(M) is for the function we are using. The function takes linear time complexity. Here, the size of the height array is M, so it will take O(M).
+
+Space Complexity: O(M), where M = total no. of columns.
+Reason: We are using a height array and a stack of size M.*/
