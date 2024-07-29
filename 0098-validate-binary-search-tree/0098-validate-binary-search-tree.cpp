4b6b4af8d5a881410ffbc0b86ee1f3e5a52178fc
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2024-07-29 19:18:08 +0530

    Time: 5 ms (81.83%), Space: 20.4 MB (34.63%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -11,17 +11,28 @@
  */
 class Solution {
 public:
+bool isValidBST(TreeNode* root) {
+    return isValidBST(root, NULL, NULL);
+}
+
+bool isValidBST(TreeNode* root, TreeNode* minNode, TreeNode* maxNode) {
+    if(!root) return true;
+    if(minNode && root->val <= minNode->val || maxNode && root->val >= maxNode->val)
+        return false;
+    return isValidBST(root->left, minNode, root) && isValidBST(root->right, root, maxNode);
+}
+};/*
+I guess he meant that any operation involved with INT_MAX and INT_MIN can easily cause numeric overflow, and signed integer overflow in C++ is Undefined Behavior, which we'd better try our best to avoid.
+
+class Solution {
+public:
     bool isValidBST(TreeNode* root) {
-        TreeNode* prev = NULL;
-        return validate(root, prev);
+        return help(root, LONG_MIN, LONG_MAX);
     }
     
-    bool validate(TreeNode* node, TreeNode* &prev) {
-        if (node == NULL) return true;
-        if (!validate(node->left, prev)) return false;
-        if (prev != NULL && prev->val >= node->val) return false;
-        prev = node;
-        return validate(node->right, prev);
+    bool help(TreeNode* root, long min, long max){
+        if(!root)   return true;
+        if(root->val <= min || root->val >= max)  return false;
+        return help(root->left, min, root->val) && help(root->right, root->val, max);
     }
-
-};
+};*/
