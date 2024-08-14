dfa0b47bcb55af2abb0d4c58e5cd102ed78ff88c
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2024-08-14 23:16:17 +0530

    Time: 0 ms (100.00%), Space: 14.1 MB (44.19%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int findTargetSumWaysUtil(vector<int>& nums, int ind, int target, vector<vector<int>> &dp){
+    // Base cases
+    if (ind == 0) {
+        if (target == 0 && nums[0] == 0)
+            return 2; // Two ways to partition: include or exclude the element
+        if (target == 0 || target == nums[0])
+            return 1; // One way to partition: include or exclude the element
+        return 0; // No way to partition
+    }
+    
+    // If the result for this index and target sum is already calculated, return it
+    if (dp[ind][target] != -1)
+        return dp[ind][target];
+        
+    // Calculate the number of ways without taking the current element
+    int notTaken = findTargetSumWaysUtil(nums, ind - 1, target, dp);
+    
+    // Calculate the number of ways by taking the current element
+    int taken = 0;
+    if (nums[ind] <= target)
+        taken = findTargetSumWaysUtil(nums, ind-1, target-nums[ind], dp);
+        
+    // Store the sum of ways in the DP array and return it
+    return dp[ind][target] = (notTaken + taken);
+    }
+    
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n = nums.size();
+            int totSum = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        totSum += nums[i];
+    }
+    
+    // Checking for edge cases
+    if (totSum - target < 0)
+        return 0; // Not possible to achieve the target sum
+    if ((totSum - target) % 2 == 1)
+        return 0; // The difference between the total sum and target sum must be even
+    
+    int s2 = (totSum - target) / 2; // Calculate the required sum for each subset
+    
+    vector<vector<int>> dp(n, vector<int>(s2 + 1, -1)); // Initialize DP table
+    return findTargetSumWaysUtil(nums, n-1, s2, dp); // Call the helper function
+        
+        
+    }
+};
