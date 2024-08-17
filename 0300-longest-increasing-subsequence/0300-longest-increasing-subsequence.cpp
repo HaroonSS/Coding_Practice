656046a5bdff095f2f37ab460eeb3497c3cb467b
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2024-08-17 19:12:43 +0530

    Time: 61 ms (75.94%), Space: 12.9 MB (86.54%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -16,7 +16,18 @@ public:
     }
  
     // Return maximum value in lis[]
-    return *max_element(lis, lis + n);
-        
+    int res = *max_element(lis, lis + n);
+        return res;
+    // int mx = res;
+    // vector<int>lisList;
+    //     for(int i = n-1;i>=0;--i){
+    //         if(lis[i] == mx){
+    //             lisList.push_back(nums[i]);
+    //             mx--;
+    //         }
+    //     }
+    // reverse(lisList.begin(), lisList.end());
+    // return lisList;
     }
-};
+};/*
+ the res will be storing the longest increasing subsequence(LIS), store it in a mx variable, and declare a list lis, Iterate over dp from n-1 to 0. If dp[I[ == nx then it's the LIS and store the v[I] in lis vector and decrease mx. Now m-1 will be the prev element which was the part of LIS and store it in lis vector and decrease mx. Do this and finally reverse the list*/
