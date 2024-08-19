4def6ea013fe02c82d78a8743cdda2ceb54e2850
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2024-08-19 23:01:29 +0530

    Time: 22 ms (95.87%), Space: 15.8 MB (96.73%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,60 +1,47 @@
-class Solution {
-public:
-int largestRectangleArea(vector < int > & histo) {
-      int n = histo.size();
-      stack < int > st;
-      int leftsmall[n], rightsmall[n];
-      for (int i = 0; i < n; i++) {
-        while (!st.empty() && histo[st.top()] >= histo[i]) {
-          st.pop();
-        }
-        if (st.empty())
-          leftsmall[i] = 0;
-        else
-          leftsmall[i] = st.top() + 1;
-        st.push(i);
-      }
-      // clear the stack to be re-used
-      while (!st.empty())
-        st.pop();
-
-      for (int i = n - 1; i >= 0; i--) {
-        while (!st.empty() && histo[st.top()] >= histo[i])
-          st.pop();
-
-        if (st.empty())
-          rightsmall[i] = n - 1;
-        else
-          rightsmall[i] = st.top() - 1;
-
-        st.push(i);
-      }
-      int maxA = 0;
-      for (int i = 0; i < n; i++) {
-        maxA = max(maxA, histo[i] * (rightsmall[i] - leftsmall[i] + 1));
-      }
-      return maxA;
-}
-    
-    int maximalRectangle(vector<vector<char>>& mat) {
-        int m = mat.size();  // Rows
-        int n = mat[0].size();  // Columns
-        int maxArea = 0;
-        vector<int> height(n, 0);
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == '1') height[j]++;
-                else height[j] = 0;
+class Solution
+{
+    public:
+        int maximalRectangle(vector<vector < char>> &matrix)
+        {
+            if (matrix.empty()) return 0;
+            const int m = matrix.size();
+            const int n = matrix[0].size();
+            
+            int left[n], right[n], height[n];
+            fill_n(left, n, 0), fill_n(right, n, n), fill_n(height, n, 0);
+            
+            int maxA = 0;
+            for (int i = 0; i < m; i++) {
+                int cur_left = 0, cur_right = n - 1;
+                
+                for (int j = 0; j < n; j++) {
+                	// compute height (can do this from either side)
+                    if (matrix[i][j] == '1') height[j]++;
+                    else height[j] = 0;
+                }
+                
+                // compute left (from left to right)
+                for (int j = 0; j < n; j++) {
+                    if (matrix[i][j] == '1') left[j] = max(left[j], cur_left);
+                    else {
+                        left[j] = 0;
+                        cur_left = j + 1;
+                    }
+                }
+                
+               	// compute right (from right to left)
+                for (int j = n - 1; j >= 0; j--) {
+                    if (matrix[i][j] == '1') right[j] = min(right[j], cur_right);
+                    else {
+                        right[j] = n - 1;
+                        cur_right = j - 1;
+                    }
+                }
+                
+               	// compute the area of rectangle (can do this from either side)
+                for (int j = 0; j < n; j++)
+                    maxA = max(maxA, (right[j] - left[j] + 1)*height[j]);
             }
-            int area = largestRectangleArea(height);
-            maxArea = max(maxArea, area);
+            return maxA;
         }
-        return maxArea;
-    }
 };
-/*
-Time Complexity: O(N * (M+M)), where N = total no. of rows and M = total no. of columns.
-Reason: O(N) for running a loop to check all rows. Now, inside that loop, O(M) is for visiting all the columns, and another O(M) is for the function we are using. The function takes linear time complexity. Here, the size of the height array is M, so it will take O(M).
-
-Space Complexity: O(M), where M = total no. of columns.
-Reason: We are using a height array and a stack of size M.*/
