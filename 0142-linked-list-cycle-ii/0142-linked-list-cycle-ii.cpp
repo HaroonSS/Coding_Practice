d30c5b7bdc9f0257612881b4279dcc0eb0b42f95
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2024-09-03 20:47:49 +0530

    Time: 0 ms (100.00%), Space: 10.2 MB (81.75%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -40,4 +40,20 @@ class Solution
            	// If no loop is found, return NULL
             return NULL;
         }
-};
+};/*https://takeuforward.org/data-structure/starting-point-of-loop-in-a-linked-list/
+Step 2: If there is a cycle, return the entry location of the cycle
+
+2.1) L1 is defined as the distance between the head point and entry point
+2.2) L2 is defined as the distance between the entry point and the meeting point
+2.3) C is defined as the length of the cycle
+2.4) n is defined as the travel times of the fast pointer around the cycle When the first encounter of the slow pointer and the fast pointer
+
+According to the definition of L1, L2 and C, we can obtain:
+
+the total distance of the slow pointer traveled when encounter is L1 + L2
+the total distance of the fast pointer traveled when encounter is L1 + L2 + n * C
+Because the total distance the fast pointer traveled is twice as the slow pointer, Thus:
+2 * (L1+L2) = L1 + L2 + n * C 
+L1 + L2 = n * C
+L1 = (n - 1) C + (C - L2)*
+It can be concluded that the distance between the head location and entry location is equal to the distance between the meeting location and the entry location along the direction of forward movement.*/
