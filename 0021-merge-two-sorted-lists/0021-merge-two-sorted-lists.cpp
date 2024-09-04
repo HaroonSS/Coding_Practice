590e9f45cfecf6f090d177cd27430131992db4d5
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2024-09-04 19:58:54 +0530

    Time: 3 ms (85.19%), Space: 19.5 MB (89.29%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -1,37 +1,46 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode *dummy, *temp;
-        dummy = new ListNode();
-        temp = dummy;
-        
-        //when both list1 and list2 isn't empty
-        while(list1 && list2){
-            if(list1->val < list2->val){
-                temp->next = list1;
-                list1 = list1->next;
-            }
-            else{
-                temp->next = list2;
-                list2 = list2->next;   
-            }
-            temp = temp->next;
-        }
-        
-        // we reached at the end of one of the list
-        if(list1) temp->next = list1;
-        if(list2) temp->next = list2;
-        
-        return dummy->next;
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+		if(l1 == NULL)
+        {
+			return l2;    // if list1 happen to be NULL,  we will simply return list2.
+		}			
+		if(l2 == NULL)
+        {
+			return l1;    // if list2 happen to be NULL, we will simply return list1.
+		} 
+		// if value pointend by l1 pointer is less than equal to value pointed by l2 pointer
+		// we wall call recursively l1 -> next and whole l2 list.
+		if(l1 -> val <= l2 -> val)
+        {
+			l1 -> next = mergeTwoLists(l1 -> next, l2);
+			return l1;
+		}
+		else
+        {
+			l2 -> next = mergeTwoLists(l1, l2 -> next);
+			return l2;            
+		}
     }
-};
+};/*
+Maintain a head and a tail pointer on the merged linked list.
+
+Then choose the head of the merged linked list by comparing the first node of both linked lists.
+
+For all subsequent nodes in both lists, you choose the smaller current node and link it to the tail of the merged list, and moving the current pointer of that list one step forward.
+
+You keep doing this while there are some remaining elements in both the lists.
+
+If there are still some elements in only one of the lists, you link this remaining list to the tail of the merged list.
+
+Initially, the merged linked list is NULL.
+
+Compare the value of the first two nodes and make the node with the smaller value the head node of the merged linked list.
+
+Since it’s the first and only node in the merged list, it will also be the tail.
+
+Then move head1 one step forward.
+
+Time Complexity O(n+m)
+Space Complexity O(n+m) this is auxiliary stack space due to recursion.*/
