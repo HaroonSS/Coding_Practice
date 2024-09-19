63f8bace009ce2b5c570698215de830b057f1442
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2024-09-19 22:55:31 +0530

    Time: 84 ms (21.60%), Space: 70.6 MB (22.37%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -37,8 +37,8 @@ class Solution
         }
 };
 /*
-
 The idea of Kadane’s algorithm is to maintain a variable max_ending_here that stores the maximum sum contiguous subarray ending at current index and a variable max_so_far stores the maximum sum of contiguous subarray found so far, Everytime there is a positive-sum value in max_ending_here compare it with max_so_far and update max_so_far if it is greater than max_so_far.
+
 -The subarray with negative sum is discarded (by assigning max_ending_here = 0 in code).
 -We carry subarray till it gives positive sum.
 */
