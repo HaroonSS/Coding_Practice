ed8075ed543934b403c549b5e902a0b6aa880b4e
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2024-09-22 21:17:56 +0530

    Time: 44 ms (87.93%), Space: 48.9 MB (33.92%) - LeetHub

---
+++ 2620-counter/2620-counter.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    let cnt = n
+    
+    return function() {
+        return cnt++
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
