747775cf2c984cf5fd72f624a42a33a92bcd4291
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2024-09-22 21:24:05 +0530

    Time: 71 ms (5.00%), Space: 51.3 MB (82.01%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let val = init
+    return {
+    increment: function(){
+        return ++val
+    },
+    decrement: function(){
+        return --val
+    },
+   reset: function(){
+       val = init
+        return val
+    }
+    }
+    
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
