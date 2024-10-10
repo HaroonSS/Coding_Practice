c0673a31ef39b747d072d36a9f7e68bae246a58b
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2024-10-10 19:23:39 +0530

    Time: 0 ms (100.00%), Space: 11.6 MB (65.81%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -4,13 +4,8 @@ public:
         
         int numsSize = nums.size();
         
-        if(numsSize == 1)
-            return 0;
-        else if(numsSize == 2)
-            return nums[0] > nums[1] ? 0 : 1;
-        
 
-        for(int i = 0; i< nums.size(); i++){
+        for(int i = 0; i< numsSize; i++){
             if((i > 0 && nums[i] > nums[i-1]) && (i == numsSize-1 ? true :nums[i] > nums[i+1])){
                 return i;
             }
