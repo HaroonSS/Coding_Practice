e827955addfe39d4c83e96f565a7084d5ae1ccbe
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2025-03-28 11:49:13 +0530

    Time: 52 ms (79.93%), Space: 54 MB (32.75%) - LeetHub

---
+++ 2821-timeout-cancellation/2821-timeout-cancellation.js
@@ -0,0 +1,45 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+const cancellable = function(fn, args, t) {
+    // call setTimeout, which is set to call fn after t amount of time
+    const timeoutId = setTimeout(() =>
+        fn(...args)
+    , t)
+
+    // define a clearTimeout
+    const cancelFn = () => clearTimeout(timeoutId);
+
+    // When/if we call the function, it will return cancelFn,
+    // and since the return line calls (and consequentially executes)
+    // cancelFn, timeout will be cancelled, thereby cancelling fn
+    return cancelFn;
+    
+};
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *       
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelTimeMs);
+ *           
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
