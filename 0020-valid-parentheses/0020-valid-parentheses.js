d4568174a9205c602c24b0a1f8c4555afd64c809
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2025-04-23 15:50:44 +0530

    Time: 0 ms (100%), Space: 55.3 MB (67.82%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+        const stack = [];
+    const map = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
+
+    for (const char of s) {
+        if (char === '(' || char === '[' || char === '{') {
+            stack.push(char);
+        } else {
+            if (stack.pop() !== map[char]) {
+                return false;
+            }
+        }
+    }
+
+    return stack.length === 0;
+    
+};
