2782b0915a00a1deade2ef19709f49bc4557ca63
Haroon Shaikh <53135840+HaroonSS@users.noreply.github.com>
2025-05-24 20:14:26 +0530

    Time: 7 ms (35.86%), Space: 58.2 MB (42.76%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -2,8 +2,20 @@
  * @param {string} s
  * @return {boolean}
  */
+// var isPalindrome = function(s) {
+//     const tempStr = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
+//     return tempStr === tempStr.split('').reverse().join('')
+    
+// };
+
 var isPalindrome = function(s) {
     const tempStr = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
-    return tempStr === tempStr.split('').reverse().join('')
+    let left = 0, right = tempStr.length-1
+
+    while(left < right){
+        if (tempStr[left++] !== tempStr[right--])
+        return false
+    }
+    return true
     
 };
